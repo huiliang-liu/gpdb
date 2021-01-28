7b505811833ff4f0b5d3dbd229b478ed725fb4f3
Huiliang Liu <huliu@pivotal.io>
2021-01-28 17:42:48 +0800

    Rename write_to_gpfdist_timeout as gpfdist_retry_timeout
    The GUC controls the time that gpdb waits before returning when it
    connects or writes to gpfdist. It handles retry times if getting an
    error from socket/network.
    So write_to_gpfdist_timeout is not an exact name and need to be renamed.

--- src/include/utils/unsync_guc_name.h
+++ src/include/utils/unsync_guc_name.h
@@ -439,7 +439,7 @@
 		"quote_all_identifiers",
 		"random_page_cost",
 		"readable_external_table_timeout",
-		"write_to_gpfdist_timeout",
+		"gpfdist_retry_timeout",
 		"repl_catchup_within_range",
 		"resource_cleanup_gangs_on_wait",
 		"resource_scheduler",
